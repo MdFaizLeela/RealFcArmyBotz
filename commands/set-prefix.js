369938968f9cec191dd4a3328315a0347533455f
MdFaizLeela <faizleela3@gmail.com>
2024-08-11 10:25:43 +0530

    Update set-prefix.js

--- commands/set-prefix.js
+++ commands/set-prefix.js
@@ -4,7 +4,7 @@ name: "setprefix",
 code: `
 $author[Done!;https://cdn.discordapp.com/attachments/795965782944120862/798540261353193472/6286_tada_animated.gif]
 $description[Okay, My New Prefix in this server is \`$message\`, You can type \`$getServerVar[prefix]help\`]
-$footer[Miu Bot | Request By @$username]
+$footer[Request By @$username]
 $color[$random[0;999999]]
 $addTimestamp
 $setServerVar[prefix;$message]
